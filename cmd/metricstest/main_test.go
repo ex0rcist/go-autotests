9c7892cddd8b00f95d39d456fc49123f1d690b6a
Eugene Shuvalov <evshuvalov@yandex.ru>
2024-10-21 22:48:13 +0300

    wtf?

--- cmd/metricstest/main_test.go
+++ cmd/metricstest/main_test.go
@@ -80,3 +80,18 @@ func TestIteration13(t *testing.T) {
 func TestIteration14(t *testing.T) {
 	suite.Run(t, new(Iteration14Suite))
 }
+
+func TestIteration15(t *testing.T) {
+	// Запускает тест-сьют для пятнадцатой итерации
+	suite.Run(t, new(Iteration15Suite))
+}
+
+func TestIteration16(t *testing.T) {
+	// Запускает тест-сьют для шестнадцатой итерации
+	suite.Run(t, new(Iteration16Suite))
+}
+
+func TestIteration17(t *testing.T) {
+	// Запускает тест-сьют для семнадцатой итерации
+	suite.Run(t, new(Iteration17Suite))
+}
